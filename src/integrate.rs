b5eb117fded15d5fcfe2800b3c64ee72705efd08
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-05-22 17:46:59 +0200

    macro for loose comparisons and some unit tests

--- src/integrate.rs
+++ src/integrate.rs
@@ -1,5 +1,5 @@
 /// # Panics
-/// This function will panic if a is greater than b or if n is 0.
+/// This function will panic if `a` is greater than `b` or if `n` is 0.
 pub fn rectangle_rule(a: f64, b: f64, n: u32, f: impl Fn(&f64) -> f64) -> f64 {
     assert!(a <= b);
     assert!(n > 0);
@@ -14,4 +14,37 @@ pub fn rectangle_rule(a: f64, b: f64, n: u32, f: impl Fn(&f64) -> f64) -> f64 {
     }
 
     sum * height
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::f64::consts::{E, PI};
+
+    macro_rules! assert_loosely_eq {
+        ($a:expr, $b:expr, $eps:expr) => {
+            if ($a - $b).abs() > $eps {
+                panic!(
+                    "assertion failed: `(left ~= right)` \
+                (left: `{}`, right: `{}`, eps: `{}`)",
+                    $a, $b, $eps
+                );
+            }
+        };
+    }
+
+    #[test]
+    fn test_rectangle() {
+        let eps = 1e-4;
+        let div = 100;
+        assert_loosely_eq!(
+            2.0 / 3.0,
+            rectangle_rule(-1.0, 1.0, div, |x| x.powf(2.0)),
+            eps
+        );
+        assert_loosely_eq!(2.0, rectangle_rule(0.0, PI, div, |x| x.sin()), eps);
+        assert_loosely_eq!(E - 1.0, rectangle_rule(0.0, 1.0, div, |x| E.powf(*x)), eps);
+        assert_loosely_eq!(2f64.ln(), rectangle_rule(1.0, 2.0, div, |x| 1.0 / x), eps);
+        assert_loosely_eq!(1.0, rectangle_rule(0.0, PI / 2.0, div, |x| x.cos()), eps);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
-use std::f64::consts::{E, PI};
-
 pub mod integrate;
 
-fn f(x: &f64) -> f64 {
-    x.powf(2f64)
-}
-
 fn main() {
-    let iterations = 100;
-
-    let res = integrate::rectangle_rule(-1.0, 1.0, iterations, f);
-    println!("{}", res);
-
-    let res = integrate::rectangle_rule(-1.0, 1.0, iterations, |x| x.powf(2f64));
-    println!("{}", res);
-
-    let res = integrate::rectangle_rule(0.0, PI, iterations, |x| x.sin());
-    println!("{}", res);
-
-    let res = integrate::rectangle_rule(0.0, 1.0, iterations, |x| E.powf(*x));
-    println!("{}", res);
-
-    let res = integrate::rectangle_rule(1.0, 2.0, iterations, |x| 1.0 / x);
-    println!("{}", res);
-
-    let res = integrate::rectangle_rule(0.0, PI / 2.0, iterations, |x| x.cos());
-    println!("{}", res);
+    println!("nothing to do for now");
 }
